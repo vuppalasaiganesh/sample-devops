4bcb9cea6bc2c92dd226c8f50623a08aed6392e1
Ubuntu <ubuntu@ip-172-31-44-131.ec2.internal>
2024-03-13 01:10:22 +0000

    my first commit

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+########################
+###author - Sai Ganesh
+
+#aws s3
+#aws lambda
+#aws lambda
+#aws IAM users
+
+aws s3 ls
+
+aws ec2 describe-instances
+
+aws lambda list-functions
+
+aws iam list-users
+
